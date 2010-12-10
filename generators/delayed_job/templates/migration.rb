a9e3ac268a0a774920d3b2a5cc9d0fa919cdde55
Sebastian <sebastian@yo.lk>
2010-12-10 23:04:36 +0100

    Updated migration generator

--- generators/delayed_job/templates/migration.rb
+++ generators/delayed_job/templates/migration.rb
@@ -7,9 +7,10 @@ class CreateDelayedJobs < ActiveRecord::Migration
       table.text     :last_error                   # reason for last failure (See Note below)
       table.datetime :run_at                       # When to run. Could be Time.zone.now for immediately, or sometime in the future.
       table.datetime :locked_at                    # Set when a client is working on this object
-      table.datetime :failed_at                    # Set when all retries have failed (actually, by default, the record is deleted instead)
       table.string   :locked_by                    # Who is working on this object (if locked)
       table.string   :unique_key
+      table.datetime :completed_at
+      table.string   :state, :default => nil
       table.timestamps
     end
 
